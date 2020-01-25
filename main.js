745a88b4331387ace34a527f799ebecf521c10d4
IanJPeek <IanJPeek@gmail.com>
2020-01-25 19:55:51 +0000

    tutorial url added

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, Menu } = require("electron");
 
+// FROM: - https://www.google.com/search?sxsrf=ACYBGNS6f6h455A5lBsY-VyoVHgkGu66_g%3A1579979522610&ei=ApMsXsT3JJSy8gKAx54I&q=electron+tutorial+for+beginners&oq=electron+tutorial+for+&gs_l=psy-ab.1.0.0j0i22i30.14575.19907..22019...3.2..4.680.4252.13j4j2j0j2j2......0....1..gws-wiz.......0i71j35i39j0i20i263j0i273.nBJ12JvZdCY#kpvalbx=_GZMsXtzQFoPGgQbD2aaIAg33
 //BOILER-PLATE (may not need "Menu" / electron?)
 
 const path = require("path");
